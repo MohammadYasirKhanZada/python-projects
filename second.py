3d142090bfdee7c96935ea49ab0e28468b0d3fc4
Yasir Khan <m.yasirkhanzada99@gmail.com>
2021-03-18 17:10:37 +0500

    boths scripts are updated

---
+++ second.py
@@ -0,0 +1,9 @@
+# Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+num = int(input('Enter any number:'))
+mod = num % 2
+
+if mod == 0:
+    print('Oh! you input an even number')
+else:
+    print("Oh! you have selected a wrong number")
