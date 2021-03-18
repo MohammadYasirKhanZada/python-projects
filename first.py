156f96ff9cb764aebbe91cf29ae7b4346680fc3f
Yasir Khan <m.yasirkhanzada99@gmail.com>
2021-03-18 16:45:09 +0500

    first.py updated

--- first.py
+++ first.py
@@ -1,6 +1,9 @@
 ### Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
 
-name = input('Enter your name:') #getting user name
-age = int(input('Enter your age:')) # getting user input
-year = str((2020 - age) + 100) #converting into string 
+# getting user name
+name = input('Enter your name:') 
+# getting user input
+age = int(input('Enter your age:')) 
+# formalizing 
+year = str((2020 - age) + 100)  
 print(name + 'will be 100 years old in the year of: ' + year)  # final output
